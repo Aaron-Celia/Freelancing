5f31f997eb19d0b0b4aa3621b82f96f0d75a0561
Aaron-Celia <card123er@gmail.com>
2023-08-02 03:40:57 -0400

    updated styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import profilePic from '../../public/profile-pic.JPG'
 export default function Home() {
   return (
    <main>
-    <Image id='profile-pic' src={profilePic} alt='Profile Picture' className='rounded-full h-40 w-40 mt-10' />
+    <Image id='profile-pic' src={profilePic} alt='Profile Picture' className='rounded-full h-60 w-60 mt-10' />
    </main>
   )
 }
