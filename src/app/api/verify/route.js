afcf31b9a16cde6ba67d27fb41d5a1a2b2c7b284
Aaron-Celia <card123er@gmail.com>
2023-08-04 01:32:39 -0400

    debugging

--- src/app/api/verify/route.js
+++ src/app/api/verify/route.js
@@ -5,17 +5,17 @@ export async function POST(request, response) {
     const body = await request.json()
     const captchaCode = body.captchaCode
     const googleResponse = await fetch(
-        "https://www.google.com/recaptcha/api/siteverify",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            params: {
-                secret: process.env.RECAPTCHA_SECRET,
-                response: captchaCode
-            }
-        })
+			"https://www.google.com/recaptcha/api/siteverify",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded"
+				},
+				body: `secret=${encodeURIComponent(
+					process.env.RECAPTCHA_SECRET
+				)}&response=${encodeURIComponent(captchaCode)}`
+			}
+		);
     const googleResponseJson = await googleResponse.json()
     if (!googleResponseJson?.success) {
         return NextResponse.json({ message: "failed", data: googleResponseJson, success: false, error: "Captcha failed" })
