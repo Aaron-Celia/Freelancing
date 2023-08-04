948e6bf556120bbad5675c391ea3cd7616a72f5a
Aaron-Celia <card123er@gmail.com>
2023-08-04 00:37:05 -0400

    moved verification to server side and included captcha code in body of client side api call

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
+// syncronous common js syntax purpousfully used here
 const client = require("twilio")(
 	process.env.ACCOUNT_SID,
 	process.env.AUTH_TOKEN
 );
 const recipient = process.env.RECIPIENT;
 import { NextResponse } from "next/server";
-import axios from 'axios'
+import axios from "axios";
 
-
-export async function POST (request, response) {
+export async function POST(request, response) {
 	try {
-		console.log('backend function ran')
-        const body = await request.json();
-        const { email, subject, message, captchaCode } = body;
+		console.log("backend function ran");
+		const body = await request.json();
+		const { email, subject, message, captchaCode } = body;
 		const googleResponse = await axios.post(
 			"https://www.google.com/recaptcha/api/siteverify",
 			{
@@ -22,26 +22,24 @@ export async function POST (request, response) {
 				}
 			}
 		);
-		if(messageResponse.status === 'accepted'){
-			if(!googleResponse.data.success){
-				return NextResponse.json({ message: null, verified: false });
-			} else {
-				const messageResponse = await client.messages
-					.create({
-						body: `You recieved a message from someone! \nEmail: ${email} \nSubject: ${subject} \nMessage:\n ${message}`,
-						from: process.env.TWILLIO_NUMBER,
-						messagingServiceSid: process.env.MESSAGING_SID,
-						to: recipient
-					})
-					.then((message) => {
-						return message;
-					});
-				if(messageResponse) {
-					return NextResponse.json({ message: messageResponse?.status})
-				}
+		if (!googleResponse.data.success) {
+			return NextResponse.json({ message: null, verified: false });
+		} else {
+			const messageResponse = await client.messages
+				.create({
+					body: `You recieved a message from someone! \nEmail: ${email} \nSubject: ${subject} \nMessage:\n ${message}`,
+					from: process.env.TWILLIO_NUMBER,
+					messagingServiceSid: process.env.MESSAGING_SID,
+					to: recipient
+				})
+				.then((message) => {
+					return message;
+				});
+			if (messageResponse) {
+				return NextResponse.json({ message: messageResponse?.status });
 			}
 		}
 	} catch (err) {
-        return NextResponse.json({ message: 'failed', error: err.message })
-    }
-};
+		return NextResponse.json({ message: "failed", error: err.message });
+	}
+}
