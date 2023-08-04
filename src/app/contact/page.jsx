670ec5a49fce3b05489d4444725a6eac8550dd61
Aaron-Celia <card123er@gmail.com>
2023-08-04 11:48:21 -0400

    contact form works

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,5 @@
 import ContactForm from "../components/ContactForm";
 
-// contact form not in git version control for security reasons on recaptcha.
 export default function page() {
   return (
     <main className='single-page'>
