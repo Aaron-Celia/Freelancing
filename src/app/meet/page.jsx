9be8ba0c959a331e39f27ff16da11e8846e1fa21
Aaron-Celia <card123er@gmail.com>
2023-08-07 16:23:53 -0400

    fixed bug

--- src/app/meet/page.jsx
+++ src/app/meet/page.jsx
@@ -1,44 +1,45 @@
-'use client';
+"use client";
 import { useEffect } from "react";
 import { InlineWidget, PopupButton } from "react-calendly";
 
-
 export default function MeetingPage() {
-    let rootEle;
-    useEffect(() => {
-        rootEle = document.getElementById('root-booking');
-    }, [])
-	return (
-		<main
-			id="root-booking"
-			className="single-page flex flex-col justify-around items-center">
-			<h3 className="text-3xl text-white mt-10">Schedule a Video Meeting</h3>
-			<div className="flex flex-col laptop:flex-row mb-40 justify-around laptop:w-screen w-[60vw] px-10 mt-20">
-				<PopupButton
-					url="https://calendly.com/aaron-r-celia/30min"
-					/*
-					 * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-					 * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-					 */
-					className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
-					rootElement={rootEle}
-					text="Schedule a 30 Minute Meeting"
-				/>
-				<PopupButton
-					url="https://calendly.com/d/4kf-fdb-mzw/initial-meeting"
-					rootElement={rootEle}
-					className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
-					text="Schedule Initial Meeting"
-				/>
-				<PopupButton
-					url="https://calendly.com/d/25c-f35-w37/progress-check-in"
-					rootElement={rootEle}
-					className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
-					text="Schedule Progress Check in"
-				/>
-			</div>
-		</main>
-	);
+	const [clientRendering, setClientRendering] = useState(false);
+	useEffect(() => {
+		setClientRendering(true);
+	}, []);
+	if (clientRendering) {
+		return (
+			<main
+				id="root-booking"
+				className="single-page flex flex-col justify-around items-center">
+				<h3 className="text-3xl text-white mt-10">Schedule a Video Meeting</h3>
+				<div className="flex flex-col laptop:flex-row mb-40 justify-around laptop:w-screen w-[60vw] px-10 mt-20">
+					<PopupButton
+						url="https://calendly.com/aaron-r-celia/30min"
+						/*
+						 * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+						 * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+						 */
+						className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
+						rootElement={window.document.getElementById("root-booking")}
+						text="Schedule a 30 Minute Meeting"
+					/>
+					<PopupButton
+						url="https://calendly.com/d/4kf-fdb-mzw/initial-meeting"
+						rootElement={window.document.getElementById("root-booking")}
+						className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
+						text="Schedule Initial Meeting"
+					/>
+					<PopupButton
+						url="https://calendly.com/d/25c-f35-w37/progress-check-in"
+						rootElement={window.document.getElementById("root-booking")}
+						className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
+						text="Schedule Progress Check in"
+					/>
+				</div>
+			</main>
+		);
+	}
 }
 
 // link for initial meeting:
