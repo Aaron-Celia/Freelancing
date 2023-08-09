d3ee88c11371365d91d849ba47112c752f521e07
Aaron-Celia <card123er@gmail.com>
2023-08-09 03:29:32 -0400

    re-did booking page

--- src/app/components/MeetingPage.jsx
+++ src/app/components/MeetingPage.jsx
@@ -1,39 +1,56 @@
-'use client';
+"use client";
 import { useRef } from "react";
 import { PopupButton } from "react-calendly";
 
 export default function MeetingPage() {
-    const rootRef = useRef(null);
+	const rootRef = useRef(null);
 	return (
 		<main
 			ref={rootRef}
-			className="meeting-page flex flex-col justify-around items-center overflow-auto">
-			<h3 className="text-3xl text-white text-center laptop:mt-40 mt-40">
-				Schedule a Video Meeting
-			</h3>
-			<div className="flex flex-col laptop:flex-row mb-40 justify-around items-center laptop:w-screen w-[90vw] px-10 mt-40 h-[80vh]">
-				<PopupButton
-					url="https://calendly.com/aaron-r-celia/30min"
-					/*
-					 * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-					 * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-					 */
-					className="text-white w-full text-center hover:bg-green-700 duration-150 text-xl px-5 py-5 rounded-xl bg-blue-600 my-5 mx-5"
-					rootElement={document.getElementById("bg")}
-					text="30 Minute Meeting"
-				/>
-				<PopupButton
-					url="https://calendly.com/d/4kf-fdb-mzw/initial-meeting"
-					rootElement={document.getElementById("bg")}
-					className="text-white w-full text-center hover:bg-green-700 duration-150 text-xl px-5 py-5 rounded-xl bg-blue-600 my-5 mx-5"
-					text="Initial Meeting"
-				/>
-				<PopupButton
-					url="https://calendly.com/d/25c-f35-w37/progress-check-in"
-					rootElement={document.getElementById("bg")}
-					className="text-white w-full text-center hover:bg-green-700 duration-150 text-xl px-5 py-5 rounded-xl bg-blue-600 my-5 phone:mb-[7.5rem] laptop:mb-5 mx-5"
-					text="Progress Check in"
-				/>
+			className="meeting-page flex flex-col justify-around items-center overflow-auto h-screen w-screen">
+			<div className="h-[80vh] w-screen flex flex-col laptop:flex-row laptop:justify-around items-center justify-around mb-40">
+				<div className="xs:mt-5 phone:mt-0 h-40 w-3/4 laptop:w-1/4 laptop:h-1/2 border rounded-xl flex flex-col justify-around items-center bg-slate-800 bg-opacity-20 text-white text-center py-2 px-2">
+					<h2 className="text-blue-200 laptop:text-3xl text-2xl">
+						Initial Meeting
+					</h2>
+					<h3 className="text-white text-center phone:text-lg laptop:text-2xl">
+						An Initial 60-minute meeting for me to learn about your vision.
+					</h3>
+					<PopupButton
+						url="https://calendly.com/d/4kf-fdb-mzw/initial-meeting"
+						rootElement={document.getElementById("bg")}
+						className="w-3/4 bg-blue-600 h-10 rounded-xl"
+						text="Schedule"
+					/>
+				</div>
+				<div className="h-40 w-3/4 laptop:w-1/4 laptop:h-1/2 border rounded-xl flex flex-col justify-around bg-slate-800 bg-opacity-20 items-center text-white text-center py-2 px-2">
+					<h2 className="text-blue-200 laptop:text-3xl text-2xl">
+						Progress Check-in
+					</h2>
+					<h3 className="text-white text-center phone:text-lg laptop:text-2xl">
+						A 30 minute check-in meeting for you to view progress.
+					</h3>
+					<PopupButton
+						url="https://calendly.com/d/25c-f35-w37/progress-check-in"
+						rootElement={document.getElementById("bg")}
+						className="w-3/4 bg-blue-600 h-10 rounded-xl"
+						text="Schedule"
+					/>
+				</div>
+				<div className="h-40 w-3/4 laptop:w-1/4 laptop:h-1/2 border rounded-xl flex flex-col bg-slate-800 bg-opacity-20 justify-around items-center text-white text-center py-2 px-2">
+					<h2 className="text-blue-200 laptop:text-3xl text-2xl">
+						Anything Else
+					</h2>
+					<h3 className="text-white text-center phone:text-lg laptop:text-2xl">
+						A 30-minute meeting to discuss anything.
+					</h3>
+					<PopupButton
+						url="https://calendly.com/aaron-r-celia/30min"
+						className="w-3/4 bg-blue-600 h-10 rounded-xl"
+						rootElement={document.getElementById("bg")}
+						text="Schedule"
+					/>
+				</div>
 			</div>
 		</main>
 	);
