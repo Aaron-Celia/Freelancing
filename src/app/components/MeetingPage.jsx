ff8f65d25c5c4cf346737aa1eb70b807ea820b89
Aaron-Celia <card123er@gmail.com>
2023-08-09 02:10:59 -0400

    using layout id for rootElement prop for popup calendar

--- src/app/components/MeetingPage.jsx
+++ src/app/components/MeetingPage.jsx
@@ -6,7 +6,6 @@ export default function MeetingPage() {
     const rootRef = useRef(null);
 	return (
 		<main
-			id="root-booking"
 			ref={rootRef}
 			className="single-page flex flex-col justify-around items-center">
 			<h3 className="text-3xl text-white mt-10">Schedule a Video Meeting</h3>
@@ -18,18 +17,18 @@ export default function MeetingPage() {
 					 * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
 					 */
 					className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
-					rootElement={document.getElementById('root-booking')}
+					rootElement={document.getElementById('bg')}
 					text="Schedule a 30 Minute Meeting"
 				/>
 				<PopupButton
 					url="https://calendly.com/d/4kf-fdb-mzw/initial-meeting"
-					rootElement={document.getElementById('root-booking')}
+					rootElement={document.getElementById('bg')}
 					className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
 					text="Schedule Initial Meeting"
 				/>
 				<PopupButton
 					url="https://calendly.com/d/25c-f35-w37/progress-check-in"
-					rootElement={document.getElementById('root-booking')}
+					rootElement={document.getElementById('bg')}
 					className="text-white text-xl px-5 py-5 rounded-xl bg-blue-600 my-10"
 					text="Schedule Progress Check in"
 				/>
